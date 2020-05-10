3ac00629804c5793f6fa3256e95f791116012342
Ariv <arivazhagan.l@blackboard.com>
2020-05-10 09:28:33 +0530

    Initial Commit

---
+++ covid19-epass-backend/src/main/java/com/ariv/cepass/Covid19EpassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ariv.cepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19EpassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19EpassBackendApplication.class, args);
+	}
+
+}
